1dce9b5f24e24c47c98dcc1239da4d75e8865951
Humberto Oliveira <109926658+humberto-oliveira@users.noreply.github.com>
2022-08-04 22:17:59 -0300

    Add files via upload

--- desafio005.py
+++ desafio005.py
@@ -16,6 +16,6 @@ print('Considerando que o número escolhido foi {}, o seu sucessor é {} e o seu
 
 # Forma 2:
 
-n = int(input('Digite um número: '))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
+# n = int(input('Digite um número: '))
+# print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
 
