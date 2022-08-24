e39a212de617dcfe69ce9707ab723ff8094d0c67
Humberto Oliveira <109926658+humberto-oliveira@users.noreply.github.com>
2022-08-24 08:04:30 -0300

    Add files via upload

---
+++ desafio021.py
@@ -0,0 +1,16 @@
+#Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
+
+from playsound import playsound
+playsound('djavan.mp3')
+
+# Como o Guanabara fez:
+
+# import pygame
+# pygame.init()
+# pygame.mixer.music.load('djavan.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
+
+
+
+
